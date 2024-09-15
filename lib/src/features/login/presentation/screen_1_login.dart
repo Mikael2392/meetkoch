5f6ebbdb5bec9682ed037154a8034b4403fc9b71
Mikael2392 <160882280+Mikael2392@users.noreply.github.com>
2024-09-15 21:24:16 +0200

    wrap mit form

--- lib/src/features/login/presentation/screen_1_login.dart
+++ lib/src/features/login/presentation/screen_1_login.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:meetkoch/src/core/presentation/app_home.dart';
+import 'package:meetkoch/src/features/Home/presentation/home_screen.dart';
 import 'package:meetkoch/src/features/registrieren/presentation/screen_1_registrieren.dart';
 import 'dart:async'; // Für den Delayed-Timer
 
@@ -22,16 +24,19 @@ class MeetKochHome extends StatefulWidget {
 }
 
 class _MeetKochHomeState extends State<MeetKochHome> {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-
-  bool _isLogoAtTop = true; // Variable zur Kontrolle der Logo-Position
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  String? _errorMessage;
+  bool _isLoading = false;
+  bool _isPasswordVisible = false; // Umschalten der Passwortsichtbarkeit
+  bool _isLogoAtTop = true;
 
   @override
   void initState() {
     super.initState();
-
-    // Starte die Animation nach einer kurzen Verzögerung (z.B. 1 Sekunde)
+    // Startet die Animation nach einer kurzen Verzögerung (z. B. 1 Sekunde)
     Future.delayed(const Duration(seconds: 1), () {
       setState(() {
         _isLogoAtTop = false;
@@ -39,12 +44,43 @@ class _MeetKochHomeState extends State<MeetKochHome> {
     });
   }
 
+  Future<void> _signIn() async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _isLoading = true;
+      });
+
+      try {
+        await _auth.signInWithEmailAndPassword(
+          email: _emailController.text.trim(),
+          password: _passwordController.text.trim(),
+        );
+
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const AppHome()),
+        );
+      } on FirebaseAuthException catch (e) {
+        setState(() {
+          _errorMessage = e.message;
+        });
+      } catch (e) {
+        setState(() {
+          _errorMessage = "Ein unerwarteter Fehler ist aufgetreten.";
+        });
+      } finally {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          // Hintergrunddekoration
           Container(
             decoration: const BoxDecoration(
               gradient: LinearGradient(
@@ -57,121 +93,146 @@ class _MeetKochHomeState extends State<MeetKochHome> {
               ),
             ),
           ),
-          // Animierter Avatar
           AnimatedPositioned(
             duration: const Duration(seconds: 2),
             curve: Curves.easeInOut,
-            top: _isLogoAtTop
-                ? -150
-                : MediaQuery.of(context).size.height *
-                    0.15, // Position animiert von oben
-            left: MediaQuery.of(context).size.width * 0.5 -
-                70, // Zentrieren des Logos
+            top:
+                _isLogoAtTop ? -150 : MediaQuery.of(context).size.height * 0.15,
+            left: MediaQuery.of(context).size.width * 0.5 - 70,
             child: const CircleAvatar(
               radius: 70,
               backgroundColor: Color(0xFFBF8AA7),
               backgroundImage: AssetImage('assets/icons/MeetKoch (2).png'),
             ),
           ),
-
           Center(
             child: SingleChildScrollView(
               padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 200),
-                  const Text(
-                    'MeetKoch',
-                    style: TextStyle(
-                      fontSize: 36,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFFFF8AA7),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 200),
+                    const Text(
+                      'MeetKoch',
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFFFF8AA7),
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          controller: emailController,
-                          decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.grey[300],
-                            hintText: 'Username, Email',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide.none,
-                            ),
-                            contentPadding: const EdgeInsets.symmetric(
-                                vertical: 15, horizontal: 20),
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        TextFormField(
-                          controller: passwordController,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.grey[300],
-                            hintText: 'Password',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide.none,
-                            ),
-                            contentPadding: const EdgeInsets.symmetric(
-                                vertical: 15, horizontal: 20),
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const AppHome(),
+                    const SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            controller: _emailController,
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.grey[300],
+                              hintText: 'E-Mail',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide.none,
                               ),
-                            );
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                                const Color.fromARGB(255, 188, 180, 133),
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 100.0, vertical: 12.0),
+                              contentPadding: const EdgeInsets.symmetric(
+                                  vertical: 15, horizontal: 20),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Bitte gib deine E-Mail ein';
+                              }
+                              return null;
+                            },
                           ),
-                          child: const Text(
-                            'Log in',
-                            style: TextStyle(color: Colors.black),
-                          ),
-                        ),
-                        const SizedBox(height: 200),
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    const RegistrationScreen1(),
+                          const SizedBox(height: 20),
+                          TextFormField(
+                            controller: _passwordController,
+                            obscureText: !_isPasswordVisible,
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.grey[300],
+                              hintText: 'Passwort',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide.none,
                               ),
-                            );
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                                const Color.fromARGB(255, 188, 180, 133),
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 100.0, vertical: 12.0),
+                              contentPadding: const EdgeInsets.symmetric(
+                                  vertical: 15, horizontal: 20),
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  _isPasswordVisible
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    _isPasswordVisible = !_isPasswordVisible;
+                                  });
+                                },
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Bitte gib dein Passwort ein';
+                              }
+                              return null;
+                            },
                           ),
-                          child: const Text(
-                            'Registrieren',
-                            style: TextStyle(color: Colors.black),
+                          const SizedBox(height: 20),
+                          _isLoading
+                              ? const CircularProgressIndicator()
+                              : ElevatedButton(
+                                  onPressed: _signIn,
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: const Color.fromARGB(
+                                        255, 188, 180, 133),
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 100.0, vertical: 12.0),
+                                  ),
+                                  child: const Text(
+                                    'Anmelden',
+                                    style: TextStyle(color: Colors.black),
+                                  ),
+                                ),
+                          const SizedBox(height: 200),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      const RegistrationScreen1(),
+                                ),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  const Color.fromARGB(255, 188, 180, 133),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 100.0, vertical: 12.0),
+                            ),
+                            child: const Text(
+                              'Registrieren',
+                              style: TextStyle(color: Colors.black),
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 5),
-                ],
+                    if (_errorMessage != null)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Text(
+                          _errorMessage!,
+                          style: const TextStyle(color: Colors.red),
+                        ),
+                      ),
+                    const SizedBox(height: 5),
+                  ],
+                ),
               ),
             ),
           ),
